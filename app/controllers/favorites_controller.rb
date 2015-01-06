155790ff3bb2bd9d180042f442113e8db02565e0
Sameet <sameetandpotatoes@gmail.com>
2015-01-05 18:15:10 -0600

    switched from session to cookies

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,26 +1,31 @@
 class FavoritesController < ApplicationController
   def index
-    @all_favs = session[:favorites]
+    # @all_favs = session[:favorites]
+    @all_favs = ActiveSupport::JSON.decode(cookies[:favorites])
   end
 
   def create
     @title = "Favorites"
-    (session[:favorites] ||= {})[params[:package]] = params[:activities]
-    @length = (session[:favorites] || {}).length
+    obj = {}
+    obj[params[:package]] = params[:activities]
+    cookies[:favorites] = { value: ActiveSupport::JSON.encode(obj), expires: 1.month.from_now }
+    @length = (ActiveSupport::JSON.decode(cookies[:favorites]) || {}).length
     render layout: false
   end
 
   def destroy
     @title = "Favorites"
-    session[:favorites].delete(params[:package_id])
-    @all_favs = session[:favorites]
-    @length = (session[:favorites] || {}).length
+    obj = ActiveSupport::JSON.decode(cookies[:favorites])
+    obj.delete(params[:package_id])
+    cookies[:favorites] = { value: ActiveSupport::JSON.encode(obj), expires: 1.month.from_now }
+    @all_favs = ActiveSupport::JSON.decode(cookies[:favorites])
+    @length = (ActiveSupport::JSON.decode(cookies[:favorites]) || {}).length
     render layout: false
   end
 
   def clear_all
-    session[:favorites] = {}
-    @all_favs = session[:favorites]
+    cookies.delete :favorites
+    @all_favs = cookies[:favorites]
     render "index"
   end
 end
